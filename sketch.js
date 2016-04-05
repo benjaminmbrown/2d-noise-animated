cb72cb61e5416f38c286af929694007bbc0eda99
Benjamin M. Brown <benjaminbb@gmail.com>
2016-04-05 11:40:29 -0500

    changing noise scale

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var noiseScale = 0.02;
+var noiseScale = 0.01;
 function setup() {
  
   createCanvas(600, 400);
@@ -9,7 +9,7 @@ function setup() {
 
 function draw() {
 	 for (var x=0; x < width; x++) {
-	    var noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+	    var noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale, 240);
 	    stroke(noiseVal*255);
 	    line(x, mouseY+noiseVal*80, x, height);
 	  }
